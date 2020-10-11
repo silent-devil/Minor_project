ff63df2f18922830e493db0c382d27f135456bc2
silent <silent@localhost.localdomain>
2020-10-11 18:15:19 +0530

    updated xss

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 from flask import Flask , render_template , request
 import portScan
 from portScan import portScanner2
+import xssscan
+from xssscan import scan_xss
 app= Flask(__name__)
 
 @app.route('/')
@@ -13,5 +15,10 @@ def ports():
     res=portScanner2(ip)
     return render_template("port.html", len=len(res) , pO=res)
 
+@app.route('/xss', methods=["GET","POST"])    
+def xss():
+    url=request.form['ipaddress']
+    result=scan_xss(url)
+    return render_template("xss_scn.html",le=len(result),ur=url,p=result)
 
 app.run(debug= True)
